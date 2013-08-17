294c9ef1a1d2f719b749cc3fef17357b4aa7ac31
nerdfiles <nerdfiles@gmail.com>
2013-08-16 20:26:50 -0500

    add api for hackers

--- frontend/views.py
+++ frontend/views.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 # from rest_framework.response import Response
 from models import Hacker
 from serializers import HackerSerializer
@@ -25,9 +27,32 @@ class HackerDetail(generics.RetrieveUpdateAPIView):
 
     @csrf_exempt
     def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated():
-            kwargs[self.slug_field] = request.user.username
+        kwargs[self.slug_field] = request.user.username
         return super(HackerDetail, self).dispatch(request, *args, **kwargs)
+
+
+class HackerAdd(generics.CreateAPIView):
+    model = Hacker
+    serializer_class = HackerSerializer
+
+    # Auth
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(
+            data=request.DATA, files=request.FILES)
+        if serializer.is_valid():
+            password = serializer.init_data.get(
+                'password', None) or Hacker.objects.make_random_password()
+            serializer.object.set_password(password)
+            self.pre_save(serializer.object)
+            self.object = serializer.save()
+            self.post_save(self.object, created=True)
+            # _user = auth.authenticate(username=self.object.username,)
+            # auth.login(request, _user)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED,
+                            headers=headers)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 def home(request):
--- heartbeat/urls.py
+++ heartbeat/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from rest_framework.urlpatterns import format_suffix_patterns
-from frontend.views import HackerDetail
+from frontend.views import HackerDetail, HackerAdd
 
 from django.contrib import admin
 admin.autodiscover()
@@ -15,7 +15,8 @@ urlpatterns = patterns('',
 urlpatterns += patterns('frontend.views',
                         url(r'^api/hacker/$',
                             HackerDetail.as_view(), name='hacker-detail'),
-                        )
+                        url(r'^api/hacker/create/$',
+                            HackerAdd.as_view(), name='customer-create'),)
 
 urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'api'])
 
