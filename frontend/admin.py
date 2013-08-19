514c20d3f2685d1f4b01efa4881ac380af269cd6
nerdfiles <nerdfiles@gmail.com>
2013-08-18 21:20:31 -0500

    fix up admin forms

--- frontend/admin.py
+++ frontend/admin.py
@@ -10,10 +10,10 @@ class ItemAdmin(admin.ModelAdmin):
     form = ItemAdminForm
 
     fieldsets = (
-                (None, {'fields': (
-                    'title',
-                    'type',
-                    'posted_date')}),
+        (None, {
+            'fields': (
+                'title', 'type',
+                'posted_date',)}),
     )
 
 
@@ -22,11 +22,19 @@ class HackerAdmin(admin.ModelAdmin):
     form = HackerAdminForm
 
     fieldsets = (
-        (None, {'fields': ('username', 'password', 'heartbeat')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions'),
-                            'classes': ['collapse']}),
-        (_('Important dates'), {'fields': ('last_login',)}),
+        (None, {
+            'fields': (
+                'username', 'password',
+                'admin', 'heartbeat',)}),
+        (_('Permissions'), {
+            'fields': (
+                'is_active', 'is_staff',
+                'is_superuser', 'groups',
+                'user_permissions'),
+            'classes': ['collapse']}),
+        (_('Important dates'), {
+            'fields': (
+                'last_login',)}),
     )
     filter_horizontal = ('groups', 'user_permissions')
 
@@ -36,7 +44,9 @@ class HeartbeatAdmin(admin.ModelAdmin):
     form = HeartbeatAdminForm
 
     fieldsets = (
-        (None, {'fields': ('items',)}),
+        (None, {
+            'fields': (
+                'items',)}),
     )
 
 admin.site.register(Item, ItemAdmin)
