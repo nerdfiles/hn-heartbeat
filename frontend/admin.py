f421637edd629b3e243fc6c4c6196e64febc4ea8
nerdfiles <nerdfiles@gmail.com>
2013-08-18 21:15:10 -0500

    configure forms

--- frontend/admin.py
+++ frontend/admin.py
@@ -1,15 +1,28 @@
 from django.contrib import admin
-from forms import HackerAdminForm
-from models import Hacker
-
+from .forms import HackerAdminForm, HeartbeatAdminForm, ItemAdminForm
+from .models import Hacker, Heartbeat, Item
 
 _ = lambda s: s
 
 
-class HackerAdmin(admin.ModelAdmin):
-    form = HackerAdminForm
+class ItemAdmin(admin.ModelAdmin):
+
+    form = ItemAdminForm
+
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
+                (None, {'fields': (
+                    'title',
+                    'type',
+                    'posted_date')}),
+    )
+
+
+class HackerAdmin(admin.ModelAdmin):
+
+    form = HackerAdminForm
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password', 'heartbeat')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions'),
                             'classes': ['collapse']}),
@@ -17,4 +30,15 @@ class HackerAdmin(admin.ModelAdmin):
     )
     filter_horizontal = ('groups', 'user_permissions')
 
+
+class HeartbeatAdmin(admin.ModelAdmin):
+
+    form = HeartbeatAdminForm
+
+    fieldsets = (
+        (None, {'fields': ('items',)}),
+    )
+
+admin.site.register(Item, ItemAdmin)
 admin.site.register(Hacker, HackerAdmin)
+admin.site.register(Heartbeat, HeartbeatAdmin)
--- frontend/forms.py
+++ frontend/forms.py
@@ -1,9 +1,23 @@
 from django import forms
-from models import Hacker
+from .models import Hacker, Heartbeat, Item
+
+
+class ItemAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = Item
+        widgets = {}
 
 
 class HackerAdminForm(forms.ModelForm):
 
     class Meta:
         model = Hacker
+        widgets = {}
+
+
+class HeartbeatAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = Heartbeat
         widgets = {}
