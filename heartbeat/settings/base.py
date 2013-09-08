ea5382c1d774d2b8df7e1ce98607858b121c566d
nerdfiles <nerdfiles@gmail.com>
2013-09-08 16:47:01 -0500

    cleanup settings

--- heartbeat/settings/base.py
+++ heartbeat/settings/base.py
@@ -19,13 +19,12 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+        # 'ENGINE': 'django.db.backends.',
+        'NAME': '',
         'USER': '',
         'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -118,7 +117,7 @@ INSTALLED_APPS = (
 
 # Django REST Framework
 REST_FRAMEWORK = {
-  'FILTER_BACKEND': 'rest_framework.filters.DjangoFilterBackend'
+    'FILTER_BACKEND': 'rest_framework.filters.DjangoFilterBackend'
 }
 
 LOGGING = {
