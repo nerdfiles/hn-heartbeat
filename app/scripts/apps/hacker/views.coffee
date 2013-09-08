9a7810f939fc114723968672a928e3f5397a29bd
nerdfiles <nerdfiles@gmail.com>
2013-09-07 19:20:09 -0500

    properly loading D3/Rickshaw; @note in view the inconsistency of variable names

--- app/scripts/apps/hacker/views.coffee
+++ app/scripts/apps/hacker/views.coffee
@@ -1,11 +1,20 @@
 # Filename: apps/hacker/views.coffee
 define [
-  'apps/hacker/templates'
-  'views/_base'
-  'msgbus'
-], (Templates, AppViews, msgBus) ->
+  "d3"
+  "rickshaw"
+  "apps/hacker/templates"
+  "views/_base"
+  "msgbus"
+], (D3, rickshaw, Templates, AppViews, msgBus) ->
   "use strict"
 
   Hacker: class View extends AppViews.ItemView
+
     template: _.template Templates["hacker.view"]
+
+    onBeforeRender: () ->
+      console.log "onBeforeRender"
+      console.log d3
+      console.log Rickshaw
+
     className: "m--hacker"
--- app/scripts/apps/hacker/views.js
+++ app/scripts/apps/hacker/views.js
@@ -2,7 +2,7 @@
   var __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-  define(['apps/hacker/templates', 'views/_base', 'msgbus'], function(Templates, AppViews, msgBus) {
+  define(["d3", "rickshaw", "apps/hacker/templates", "views/_base", "msgbus"], function(D3, rickshaw, Templates, AppViews, msgBus) {
     "use strict";
     var View, _ref;
     return {
@@ -15,6 +15,12 @@
         }
 
         View.prototype.template = _.template(Templates["hacker.view"]);
+
+        View.prototype.onBeforeRender = function() {
+          console.log("onBeforeRender");
+          console.log(d3);
+          return console.log(Rickshaw);
+        };
 
         View.prototype.className = "m--hacker";
 
